f79644b89dbf979f36052111cffe9f46199f320c
Saif Alhaider <saifharith90@gmail.com>
2023-07-13 14:52:04 +0300

    add: primary button composable

---
+++ app/src/main/java/com/example/xogame/ui/screen/composables/ButtonPrimary.kt
@@ -0,0 +1,29 @@
+package com.example.xogame.ui.screen.composables
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.xogame.ui.theme.XOGameCustomColors
+
+@Composable
+fun PrimaryButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            XOGameCustomColors.current.primaryPink.copy(alpha = .7f)
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Start Game",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+    }
+}
