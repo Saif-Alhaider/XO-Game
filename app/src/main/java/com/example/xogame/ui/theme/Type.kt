ec5859ed1d0d5185555e06610f2582cd56dbb01b
Saif Alhaider <saifharith90@gmail.com>
2023-07-13 12:26:11 +0300

    add: font styles

--- app/src/main/java/com/example/xogame/ui/theme/Type.kt
+++ app/src/main/java/com/example/xogame/ui/theme/Type.kt
@@ -2,19 +2,38 @@ package com.example.xogame.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.xogame.R
 
 // Set of Material typography styles to start with
+
+private val Chewy = FontFamily(
+    Font(R.font.chewy_regular,FontWeight.Normal)
+)
+private val Poppins = FontFamily(
+    Font(R.font.poppins_regular,FontWeight.Normal),
+    Font(R.font.poppins_medium,FontWeight.Medium),
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = Chewy,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 40.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
