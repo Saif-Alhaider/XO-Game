cd0d77f56d86c04ee9a50faa317e59ca4e3db039
Saif Alhaider <saifharith90@gmail.com>
2023-07-13 13:06:35 +0300

    finish: structure

---
+++ app/src/main/java/com/example/xogame/XOGameApplication.kt
@@ -0,0 +1,8 @@
+package com.example.xogame
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class XOGameApplication:Application() {
+}
--- app/src/main/java/com/example/xogame/ui/MainActivity.kt
+++ app/src/main/java/com/example/xogame/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.xogame
+package com.example.xogame.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.xogame.ui.theme.XOGameTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
