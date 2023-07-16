106911653f5b305d0bbe755b35583eefbf642705
Saif Alhaider <saifharith90@gmail.com>
2023-07-16 16:41:40 +0300

    add: XOSocketService & its implementation

---
+++ app/src/main/java/com/example/xogame/data/remote/XOSocketService.kt
@@ -0,0 +1,23 @@
+package com.example.xogame.data.remote
+
+import com.example.xogame.data.Game
+import com.example.xogame.util.ResponseResult
+import kotlinx.coroutines.flow.Flow
+
+
+interface XOSocketService {
+    suspend fun initSession(
+        username: String
+    ): ResponseResult<String>
+
+    suspend fun joinSession(
+        username: String,
+        roomId: String,
+    ): ResponseResult<Unit>
+
+    suspend fun observeGame(): Flow<Game>
+
+    suspend fun sendXO(game:Game)
+
+    suspend fun closeSession()
+}
