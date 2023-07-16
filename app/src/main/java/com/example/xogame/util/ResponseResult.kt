e2ce47390fe51c834adc000fb16f7559960f331a
Saif Alhaider <saifharith90@gmail.com>
2023-07-16 16:42:26 +0300

    add: ResponseResult

---
+++ app/src/main/java/com/example/xogame/util/ResponseResult.kt
@@ -0,0 +1,7 @@
+package com.example.xogame.util
+
+sealed class ResponseResult<T>(val data: T? = null, val error: String? = null) {
+    class Success<T>(data: T): ResponseResult<T>(data = data)
+
+    class Error<T>(error: String): ResponseResult<T>(error = error)
+}
