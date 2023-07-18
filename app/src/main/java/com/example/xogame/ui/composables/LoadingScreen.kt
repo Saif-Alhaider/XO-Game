8f7d432437ec2e15b21cc75755be2d7c294c764a
Saif Alhaider <saifharith90@gmail.com>
2023-07-18 18:56:57 +0300

    add: lottie

---
+++ app/src/main/java/com/example/xogame/ui/composables/LoadingScreen.kt
@@ -0,0 +1,47 @@
+package com.example.xogame.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.xogame.R
+import com.example.xogame.ui.theme.XOGameTheme
+
+@Composable
+fun LoadingScreen() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    XoScaffold {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text ="Loading to enter the game", style = MaterialTheme.typography.bodyMedium)
+            LottieAnimation(
+                composition = composition, iterations = LottieConstants.IterateForever,
+                modifier = Modifier.size(300.dp),
+
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoadingScreenPreview() {
+    XOGameTheme {
+        LoadingScreen()
+    }
+}
