d10939d73d9cef83d59c76224415922579b27772
Saif Alhaider <saifharith90@gmail.com>
2023-07-18 12:37:15 +0300

    add: on click join

--- app/src/main/java/com/example/xogame/ui/screen/join_game/JoinGame.kt
+++ app/src/main/java/com/example/xogame/ui/screen/join_game/JoinGame.kt
@@ -1,37 +1,65 @@
 package com.example.xogame.ui.screen.join_game
 
+import android.util.Log
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.xogame.R
 import com.example.xogame.ui.common.composables.MainBackground
 import com.example.xogame.ui.common.composables.OutlinedTextFieldPrimary
 import com.example.xogame.ui.screen.home.composables.PrimaryButton
 import com.example.xogame.ui.theme.XOGameCustomColors
 import com.example.xogame.ui.theme.XOGameTheme
+import com.example.xogame.util.createToast
 
 @Composable
-fun JoinGameScreen() {
-    JoinGameContent()
+fun JoinGameScreen(viewModel: JoinGameViewModel = hiltViewModel(), navController: NavController) {
+    val state = viewModel.state.collectAsState().value
+    JoinGameContent(
+        updateRoomId = viewModel::updateRoomId,
+        onClickJoin = viewModel::onClickJoin,
+        onNavigateBack = {
+            navController.popBackStack()
+            viewModel.closeSession()
+        },
+        state = state
+    )
 }
 
 @Composable
-fun JoinGameContent() {
+fun JoinGameContent(
+    updateRoomId: (String) -> Unit,
+    onClickJoin: () -> Unit,
+    onNavigateBack: () -> Unit,
+    state: JoinGameUiState
+) {
+    val context = LocalContext.current
+
+    if (state.isJoined) {
+        Log.i("gg", "JoinGameContent: navigate to game")
+    }
+    BackHandler {
+        onNavigateBack()
+    }
     Box(Modifier.background(XOGameCustomColors.current.background)) {
         MainBackground()
         Box(
@@ -55,15 +83,29 @@ fun JoinGameContent() {
                 OutlinedTextFieldPrimary(
                     modifier = Modifier
                         .padding(top = 16.dp)
-                        .height(56.dp).fillMaxWidth(),
-                    value = "",
-                    enabled = true
+                        .height(56.dp)
+                        .fillMaxWidth(),
+                    value = state.roomId,
+                    enabled = true,
+                    onValueChanged = updateRoomId
                 )
             }
             PrimaryButton(
-                onClick = {},
+                onClick = {
+                    when {
+                        state.roomId.isBlank() -> {
+                            createToast(context, "please fill text field first")
+                        }
+//                        !isValidUUID(state.roomId) -> {
+//                            createToast(context, "room id is not valid")
+//                        }
+                        else -> onClickJoin()
+                    }
+                },
                 text = "Join Game",
-                modifier = Modifier.padding(bottom = 40.dp).align(Alignment.BottomCenter)
+                modifier = Modifier
+                    .padding(bottom = 40.dp)
+                    .align(Alignment.BottomCenter)
             )
         }
 
@@ -74,6 +116,6 @@ fun JoinGameContent() {
 @Composable
 fun JoinGamePreview() {
     XOGameTheme {
-        JoinGameScreen()
+        JoinGameScreen(navController = rememberNavController())
     }
 }
