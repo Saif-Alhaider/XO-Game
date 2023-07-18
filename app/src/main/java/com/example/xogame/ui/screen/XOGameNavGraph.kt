482dc01a139738dc3bd2e3f51a4c418886fc0d29
Saif Alhaider <saifharith90@gmail.com>
2023-07-18 10:23:15 +0300

    add: navgraph

---
+++ app/src/main/java/com/example/xogame/ui/screen/XOGameNavGraph.kt
@@ -0,0 +1,15 @@
+package com.example.xogame.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.xogame.ui.screen.home.homeRoute
+import com.example.xogame.ui.screen.start_game.startGameRoute
+
+@Composable
+fun XOGameNavGraph(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = AppDestination.Home.route) {
+        homeRoute(navHostController)
+        startGameRoute(navHostController)
+    }
+}
