9acafe03dc9ad4d788fc2b35a9f1309d0ba663ad
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-07-18 14:12:56 +0300

    edit name of datastore

--- app/src/main/java/com/example/xogame/data/local/PlayerDatastore.kt
+++ app/src/main/java/com/example/xogame/data/local/PlayerDatastore.kt
@@ -1,6 +1,6 @@
 package com.example.xogame.data.local
 
-interface PlayerDatasource {
+interface PlayerDatastore {
     suspend fun savePlayerName(playerName: String)
     suspend fun getPlayerName(): String?
     suspend fun clearPlayerName()
