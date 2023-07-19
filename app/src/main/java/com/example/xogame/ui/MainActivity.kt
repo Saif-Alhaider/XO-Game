06637a6f54f25a82a51ded5b656a5e7802808cbb
Saif Alhaider <saifharith90@gmail.com>
2023-07-19 18:03:38 +0300

    edit: status bar color

--- app/src/main/java/com/example/xogame/ui/MainActivity.kt
+++ app/src/main/java/com/example/xogame/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.xogame.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.xogame.ui.screen.XOGameNavGraph
+import com.example.xogame.ui.theme.XOGameCustomColors
 import com.example.xogame.ui.theme.XOGameTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +19,8 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             XOGameTheme {
+                val systemUiColor = rememberSystemUiController()
+                systemUiColor.setStatusBarColor(color = XOGameCustomColors.current.background)
                 val navController = rememberNavController()
                 XOGameNavGraph(navHostController = navController)
             }
