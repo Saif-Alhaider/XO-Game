0fc315de525e9f1c3397dd872beae504c664e89e
Saif Alhaider <saifharith90@gmail.com>
2023-07-19 12:07:23 +0300

    add: xo logic

--- app/src/main/java/com/example/xogame/ui/screen/play/PlayGameViewModel.kt
+++ app/src/main/java/com/example/xogame/ui/screen/play/PlayGameViewModel.kt
@@ -11,7 +11,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -44,9 +43,18 @@ class PlayGameViewModel @Inject constructor(
         } catch (e: Exception) {
             Log.e("TAG", "init error: ${e.message}")
         }
+        observeWinning()
     }
 
-    fun disablePosition(){
+    private fun observeWinning() {
+        viewModelScope.launch {
+            _state.collectLatest {
+                Log.i("gg", "winning pos:${winningPositions(_state.value.board)} ")
+            }
+        }
+    }
+
+    fun disablePosition() {
         _state.update {
             it.copy(isActive = false)
         }
@@ -78,5 +86,58 @@ class PlayGameViewModel @Inject constructor(
         viewModelScope.launch {
             xoSocketService.closeSession()
         }
+    }
+
+    private fun winningPositions(list: List<List<String>>): List<List<Int>> {
+        val winningRows = mutableListOf<List<Int>>()
+        val winningColumns = mutableListOf<List<Int>>()
+        val diagonal = mutableListOf<List<Int>>()
+
+        // horizontal
+        for ((rowIndex, row) in list.withIndex()) {
+            if (row.all { it.isNotBlank() && it == row[0] })
+                winningRows.addAll(
+                    listOf(
+                        listOf(rowIndex, 0),
+                        listOf(rowIndex, 1),
+                        listOf(rowIndex, 2)
+                    )
+                )
+        }
+        //vertical
+        for ((colIndex, _) in list.withIndex()) {
+            if (list.all { it[colIndex].isNotBlank() && it[colIndex] == list[0][colIndex] }) {
+                winningColumns.addAll(
+                    listOf(
+                        listOf(0, colIndex),
+                        listOf(1, colIndex),
+                        listOf(2, colIndex)
+                    )
+                )
+            }
+        }
+
+        if ((list[0][0] == list[1][1] && list[1][1] == list[2][2]) && (list[0][0].isNotBlank()
+                    && list[1][1].isNotBlank() && list[2][2].isNotBlank())
+        )
+            diagonal.addAll(
+                listOf(
+                    listOf(0, 0),
+                    listOf(1, 1),
+                    listOf(2, 2)
+                )
+            )
+        if ((list[0][2] == list[1][1] && list[1][1] == list[2][0]) && (list[0][2].isNotBlank()
+                    && list[1][1].isNotBlank() && list[2][0].isNotBlank())
+        )
+            diagonal.addAll(
+                listOf(
+                    listOf(0, 2),
+                    listOf(1, 1),
+                    listOf(2, 0)
+                )
+            )
+
+        return winningRows + winningColumns + diagonal
     }
 }
