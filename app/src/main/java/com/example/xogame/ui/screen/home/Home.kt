01551a9dc8f0ef884b6a9447c47c8d2cf8f48621
Saif Alhaider <saifharith90@gmail.com>
2023-07-20 12:32:26 +0300

    refactor: home

--- app/src/main/java/com/example/xogame/ui/screen/home/Home.kt
+++ app/src/main/java/com/example/xogame/ui/screen/home/Home.kt
@@ -4,9 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -62,7 +60,6 @@ fun HomeContent(
         Box(
             Modifier
                 .fillMaxSize()
-
         ) {
             Column(
                 verticalArrangement = Arrangement.Center,
